1565bfc73c3c82ab3e4d98252f7b1791afdc7bb4
Mayur Mudoi <mayurmudoi.personal@gmail.com>
2023-10-26 13:29:00 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import 'node-libs-react-native/globals';
+// import 'intl-pluralrules';
+import 'react-native-url-polyfill/auto';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { MMSDK } from './web3/metamask';
+
+export default function App() {
+	const onPress = async () => {
+		await MMSDK.connect();
+		console.log("connected")
+		const ethereum = MMSDK.getProvider();
+		const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+		console.log(accounts);
+	};
+
+	return (
+		<View style={styles.container}>
+			<Button
+				title='Sign up with metamask'
+				onPress={onPress}
+			/>
+			<StatusBar style='auto' />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+});
